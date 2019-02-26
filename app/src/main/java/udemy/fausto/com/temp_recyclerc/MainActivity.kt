df90bd2e0ed3ca87f8cdeba239524d44f0f041a0
Fausto Checa <fausto.checa@gmail.com>
2019-02-26 12:29:24 +0100

    First

---
+++ app/src/main/java/udemy/fausto/com/temp_recyclerc/MainActivity.kt
@@ -0,0 +1,13 @@
+package udemy.fausto.com.temp_recyclerc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/udemy/fausto/com/temp_recyclerc/Person.kt
@@ -0,0 +1,7 @@
+package udemy.fausto.com.temp_recyclerc
+
+import java.io.Serializable
+
+class Person (val name: String, val age: String, val image: String): Serializable {
+
+}
